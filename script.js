aaf824e35b8dc50b8e0cfa81b9bf0f8a29205ecd
Abdelkader <abdojit8@gmail.com>
2024-10-08 17:26:59 +0000

    close button

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
+
 function run(){
   try{
-      var ht =    document.getElementById("code-ht").value;
-      var cs = document.getElementById("code-cs").value;
-      var js = document.getElementById("code-js").value;
-      var out = document.getElementById("out");
-      var er = document.getElementById("er");
+    var ht = document.getElementById("code-ht").value;
+    var cs = document.getElementById("code-cs").value;
+    var js = document.getElementById("code-js").value;
+    var out = document.getElementById("out");
+    var out2 = document.getElementById("out2");
+    var er = document.getElementById("er");
       out.contentDocument.body.innerHTML = ht + "<style>" + cs + "</style>";
       out.contentWindow.eval(js);
-      localStorage.setItem(code-ht,ht.value);
-      localStorage.setItem(code-cs,cs.value);
-      localStorage.setItem(code-js,js.value);
+      out2.contentDocument.body.innerHTML = ht + "<style>" + cs + "</style>";
+      out2.contentWindow.eval(js);
   }catch(error){
       console.log(error);
-      er.innerHTML = "'" + js + "'" + "<h3 id='er'>" + error + "</h3>";
+      er.innerHTML = "<h3 id='er'>" + error + "</h3>";
   }
   
   }
@@ -21,5 +22,7 @@ function cls(){
     var er = document.getElementById("er");
     er.innerHTML = "<h3 id='ner'>errors</h3>";
     out.contentDocument.body.innerHTML = "";
-    //er.value = "";
+}
+functionfull(){
+    var com = ocument.getElementById("full");
 }
